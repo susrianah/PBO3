6ae6ee7725f8f2398d2e6b92aac3fae625719f9b
susrianah <susriana859@gmail.com>
2017-10-27 19:28:19 +0700

    mengisi attribute dari objek"m"

--- src/pbo3/PBO3.java
+++ src/pbo3/PBO3.java
@@ -2,6 +2,10 @@ package pbo3;
 public class PBO3 {
     public static void main(String[] args) {
         myFrame m = new myFrame();
+        
+        m.pesan = "Halo...";
+        m.bilBulat = 5;
+        m.bilDesimal = 3.14;
     }
     
 }
